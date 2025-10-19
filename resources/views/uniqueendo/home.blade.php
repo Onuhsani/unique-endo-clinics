6c25d28a777f015f3a14467a52d2d3fcf1948dc3
Makaveli <talk2onuh@gmail.com>
2025-10-19 09:04:21 +0100

    Edited the about page

--- resources/views/uniqueendo/home.blade.php
+++ resources/views/uniqueendo/home.blade.php
@@ -293,7 +293,7 @@
             <div class="row">
                 <div class="col-lg-4 col-md-6 col-sm-6">
                     <div class="team__item">
-                        <img src="{{ asset('img/logo.jpg') }}" alt="">
+                        <img src="{{ asset('img/Logo.jpg') }}" alt="">
                         <h5>Dr. Unique</h5>
                         <span>Surgeon</span>
                         <div class="team__item__social">
@@ -306,7 +306,7 @@
                 </div>
                 <div class="col-lg-4 col-md-6 col-sm-6">
                     <div class="team__item">
-                        <img src="{{ asset('img/logo.jpg') }}" alt="">
+                        <img src="{{ asset('img/Logo.jpg') }}" alt="">
                         <h5>Dr. Unique</h5>
                         <span>Surgeon</span>
                         <div class="team__item__social">
@@ -319,7 +319,7 @@
                 </div>
                 <div class="col-lg-4 col-md-6 col-sm-6">
                     <div class="team__item">
-                        <img src="{{ asset('img/logo.jpg') }}" alt="">
+                        <img src="{{ asset('img/Logo.jpg') }}" alt="">
                         <h5>Dr. Unique</h5>
                         <span>Surgeon</span>
                         <div class="team__item__social">
